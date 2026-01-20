e84cb43823518a482d7ef68ab986489738ae2e03
Emmanuel Boillot <emmanuel.boillot@etu.minesparis.psl.eu>
2026-01-20 15:44:45 +0100

    Debut data

---
+++ datas.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+class DatabaseManager:
+    def __init__ (self):
+        pass
+
+    def connexion_SQLite(self):
+        pass
+
+    def creation_tables(self):
+        pass
+
+    def insertion_annonces(self):
+        pass
+
+    def lecture_donnees(self):
+        pass
+
+    def fermeture_base(self):
+        pass
