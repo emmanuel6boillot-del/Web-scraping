0a5a9667058922860b539e545b13bef48c2e2619
Gabriel Chaleil <gabriel.chaleil@etu.minesparis.psl.eu>
2026-01-21 16:02:39 +0100

    ScrappingVF

--- scraping.py
+++ scraping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = "https://books.toscrape.com/"
 response = requests.get(url)
@@ -12,12 +13,47 @@ soup = BeautifulSoup(html, "html.parser")
 
 data = {}
 
-livres = soup.find_all("article", class_="product_pod") #liste de tout les livres
+def trad_score(m : str) :
+    if m == "One" :
+        return 1
+    elif m == "Two" :
+        return 2
+    elif m == "Three" :
+        return 3
+    elif m == "Four" :
+        return 4
+    elif m == "Five" :
+        return 5
 
-for x in livres :
-    titre = livre.h3.a["title"]
-    try:
-        prix = livre.find("p", class_="price_color").text
-    except AttributeError:
-        prix = None
-    data[titre] = [prix, ]
+liens = soup.find("ul", class_="nav nav-list")
+liens1 = liens.find("ul")
+links = liens1.find_all("li")
+
+for x in links :
+
+    new_url = f"https://books.toscrape.com/{x.a["href"]}"
+    catégorie = x.text.strip()
+    response = requests.get(new_url)
+    html = response.text
+    if response.status_code != 200:
+        print("Erreur :", response.status_code)
+    new_soup = BeautifulSoup(html, "html.parser")
+
+    livres = new_soup.find_all("article", class_="product_pod") #liste de tout les livres de la catégorie
+
+    for livre in livres :
+        titre = livre.h3.a["title"]
+        try:
+            note = trad_score(livre.find("p", class_="star-rating")["class"][1])
+        except TypeError:
+            note = None
+        
+        try:
+            prix = livre.find("p", class_="price_color").text
+        except AttributeError:
+            prix = None
+
+        data[titre] = {"prix" : float(prix[2:]) , "note" : note, "catégorie" : catégorie }
+
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
