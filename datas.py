86a9aadfbcd0498482ce3904e7ebfa62ce1a530f
Emmanuel Boillot <emmanuel.boillot@etu.minesparis.psl.eu>
2026-01-21 16:16:35 +0100

    data nettoyer

--- datas.py
+++ datas.py
@@ -1,26 +1,5 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-#db = DatabaseManager("database.db")
-cursor.execute("CREATE TABLE IF NOT EXISTS Books (id INTEGER PRIMARY KEY AUTOINCREMENT, titre TEXT NOT NULL, prix TEXT, note TEXT NULL, catégorie TEXT)")
-conn.commit()
-
-cursor.execute("INSERT INTO Books (titre,prix,note,catégorie) VALUES (?,?,?,?)",("Livre","5","3.5","Horreur"))
-conn.commit()
-conn.close()
-
-# Exécuter une requête
-
-
-
-import sqlite3
-
-#connexion
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-
-#Classe de la database
 class DatabaseManager:
     def __init__ (self,nom_fichier):
         self.nom = nom_fichier
